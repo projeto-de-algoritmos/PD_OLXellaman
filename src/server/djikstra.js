32a2bf86d03087a0b218903a77650226258d9931
Icaro Pereira <icarooliv2@gmail.com>
2021-08-22 19:51:48 -0300

    Adds data manipulation functions
    Co-authored-by: Augusto Moreno Vilarins <augustovilarins@gmail.com>

---
+++ src/server/djikstra.js
@@ -0,0 +1,161 @@
+const {Graph, Node} = require('./models');
+
+const buildGraph = () => {
+  let graph = new Graph();
+
+  let book = new Node('book');
+  let poster = new Node("poster");
+  let rare_lp = new Node("rare lp");
+  let bass_guitar = new Node("bass guitar");
+  let piano = new Node("piano");
+  let drumset = new Node("drumset");
+
+  graph.addNode(book);
+  graph.addNode(poster);
+  graph.addNode(rare_lp);
+  graph.addNode(bass_guitar);
+  graph.addNode(piano);
+  graph.addNode(drumset);
+
+  graph.addEdge(book, poster, 0);
+  graph.addEdge(book, rare_lp, 5);
+  graph.addEdge(rare_lp, bass_guitar, 15);
+  graph.addEdge(rare_lp, drumset, 20);
+  graph.addEdge(poster, bass_guitar, 30);
+  graph.addEdge(poster, drumset, 35);
+  graph.addEdge(bass_guitar, piano, 20);
+  graph.addEdge(drumset, piano, 10);
+
+  return graph;
+}
+
+
+
+const dijkstra = (graph, source) => {
+  const minDist = (priorityQueue, dist) => {
+    let minNode = null;
+    for (let node of priorityQueue) {
+      if (minNode == null) {
+        minNode = node;
+      } else if (dist[node] < dist[minNode]) {
+        minNode = node;
+      }
+      return minNode;
+    }
+  };
+
+  let q = [];
+  let dist = {};
+  let prev = {};
+  for (let node of graph.nodes) {
+    dist[node.id] = Infinity;
+    dist[node.id] = Infinity;
+    q.push(node.id);
+  }
+
+  dist[source.id] = 0;
+
+  while ([...q].length > 0) {
+    let u = graph.getNode(minDist(q, dist));
+    q = q.filter((id) => id != u.id);
+
+    if (u.id in graph.edges) {
+      for (let [_, v] of Object.entries(graph.edges[u.id])) {
+        let alt = dist[u.id] + v.length;
+        if (alt < dist[v.to.id]) {
+          dist[v.to.id] = alt;
+          prev[v.to.id] = u;
+        }
+      }
+    }
+  }
+  return {
+    dist,
+    prev
+  };
+};
+
+const toArray = (prev, fromNode) => {
+  let previousNode = prev[fromNode.id];
+  let route = [fromNode.id];
+  while (previousNode != undefined) {
+    route.push(previousNode.id);
+    let temp = previousNode;
+    previousNode = prev[temp.id];
+  }
+  route.reverse();
+  return route;
+};
+
+const addTrade = (graph, item) => {
+  const fromNode = graph.getNode(item.fromId);
+  const toNode = new Node(item.name);
+  graph.addNode(toNode);
+  graph.addEdge(toNode, fromNode, item.tradeTax);
+}
+
+const calcTrade = (graph, fromId, toId) => {
+  const fromNode = graph.getNode(fromId);
+  const toNode = graph.getNode(toId);
+  const {
+    dist,
+    prev
+  } = dijkstra(graph, fromNode);
+  console.log({dist});
+  const route = toArray(prev, toNode);
+  const total = dist[toNode.id];
+  const nodes = route.map((id) => graph.getNode(id));
+  return {
+    nodes,
+    total
+  };
+}
+
+const doTrade = (graph, item) => {
+  const {
+    route,
+    total
+  } = calcTrade(item);
+  const fromNode = graph.getNode(item.fromId);
+  const toNode = graph.getNode(item.toId);
+  graph.removeNode(fromNode);
+  graph.removeNode(toNode);
+  return {
+    route,
+    total
+  }
+}
+
+const getAllTrades = (graph) => {
+  const trades = [];
+  for (let [_, edge] of Object.entries(graph.edges)) {
+    for (let [_, v] of Object.entries(edge)) {
+      trades.push({
+        from: v.from,
+        to: v.to,
+        tradeTax: v.length
+      });
+    }
+  }
+  return trades;
+}
+
+const getAllItems = (graph) => {
+  const items = [];
+  for (let node of graph.nodes) {
+    items.push({
+      id: node.id,
+      name: node.name
+    });
+  }
+  return items;
+}
+
+module.exports = {
+    addTrade,
+    doTrade,
+    calcTrade,
+    getAllTrades,
+    getAllItems,
+    buildGraph
+}
