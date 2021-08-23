2162cc5124030e801f1a80795eef6443a08876fe
Icaro Pereira <icarooliv2@gmail.com>
2021-08-22 22:49:44 -0300

    Loads graph dynamically
    Co-authored-by: Augusto Moreno Vilarins <augustovilarins@gmail.com>

--- src/App.js
+++ src/App.js
@@ -4,31 +4,14 @@ import './App.css';
 import ItemForm from './ItemForm';
 import {ItemTable} from './ItemTable';
 
-const App = () => {
-  const trocas = [
-    {from: {id: 1, name: "Item 1"}, to: {id: 2, name: "Item 2"}, tradeTax: 20},
-    {from: {id: 3, name: "Item 3"}, to: {id: 4, name: "Item 4"}, tradeTax: 2},
-    {from: {id: 3, name: "Item 4"}, to: {id: 5, name: "Item 5"}, tradeTax: 3},
-    {from: {id: 6, name: "Item 6"}, to: {id: 7, name: "Item 7"}, tradeTax: 4},
-  ];
-  
-  const items = [
-      {name: "Item 1", id: 1},
-      {name: "Item 2", id: 2},
-      {name: "Item 3", id: 3}, 
-      {name: "Item 4", id: 4},
-      {name: "Item 5", id: 5},
-      {name: "Item 6", id: 6}, 
-      {name: "Item 7", id: 7},
-  ]
+const App = ({graph}) => {
+  const updateLocalTrades = newTrade => {
+  }
 
   return (
     <Container className="p-3">
     <Jumbotron>
-      <h1 className="header">Faça suas trocas</h1>
-     <ItemForm items={items} />
-    </Jumbotron>
-    <div>Lista de items</div>
+        <ItemForm items={localItems} updateLocalTrades={updateLocalTrades}/>
     <ItemTable trocas={trocas}/>
   </Container>
   )
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
 // Importing the Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {buildGraph} from './server/djikstra';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const graph = buildGraph();
+
+ReactDOM.render(<App graph={graph}/>, document.getElementById('root'));
