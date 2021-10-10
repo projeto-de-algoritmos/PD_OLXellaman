9a34dcc9951f0253bf6503187e714ef17f403898
Icaro Pereira <icarooliv2@gmail.com>
2021-10-10 17:25:42 -0300

    Adapts calcBestPrice to use bellmanFord algo and updates messages
    Co-authored-by: Augusto Moreno Vilarins <augustovilarins@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,43 +1,80 @@
-import React, { useState} from 'react';
-import {Jumbotron, Container} from 'react-bootstrap';
-import './App.css';
-import ItemForm from './ItemForm';
-import {ItemTable} from './ItemTable';
-import ItemCalc from './ItemCalc';
-import {getAllItems, getAllTrades, addTrade, calcTrade} from './server/djikstra';
+import React, { useState } from "react";
+import { Jumbotron, Container } from "react-bootstrap";
+import "./App.css";
+import ItemForm from "./ItemForm";
+import { ItemTable } from "./ItemTable";
+import ItemCalc from "./ItemCalc";
+import { getAllItems, getAllTrades, addTrade, calcTrade } from "./server/util";
 
-const App = ({graph}) => {
+const App = ({ graph }) => {
   const [localGraph, setLocalGraph] = useState(graph);
   const [localItems, setLocalItems] = useState(getAllItems(graph));
   const [localTrades, setLocalTrades] = useState(getAllTrades(graph));
   const [bestDeal, setBestDealSteps] = useState([]);
+  const [error, setError] = useState("");
 
-  const updateLocalTrades = newTrade => {
+  const updateLocalTrades = (newTrade) => {
     const obj = addTrade(localGraph, newTrade);
     setLocalGraph(obj.graph);
     setLocalTrades(getAllTrades(obj.graph));
     setLocalItems(getAllItems(obj.graph));
-  }
+  };
 
-  const calcBestPrice = ({fromId, toId}) => {
-    const {trades} = calcTrade(localGraph, fromId, toId);
-    const steps = [];
+  const calcBestPrice = ({ fromId, toId }) => {
+    const { trades, error } = calcTrade(
+      localGraph,
+      fromId,
+      toId,
+      "bellmanFord"
+    );
+
+    if (error) {
+      setError(error.message);
+      return;
+    }
+
+    if (trades.length === 0) {
+      setError("Não é possível fazer a troca");
+      return;
+    }
+
     const total = trades.reduce((acc, curr) => curr.tradeTax + acc, 0);
-    trades.map(trade => {
-      steps.push(`Troque o seu ${trade.from} por ${trade.to} com um adicional de ${trade.tradeTax}`);
+    const steps = trades.map((trade) => {
+      const { from, to, tradeTax } = trade;
+      if (tradeTax > 0) {
+        return `Troque o seu ${from} por ${to} pagando ${tradeTax}`;
+      }
+      return `Troque o seu ${from} por ${to} recebendo ${tradeTax * -1}`;
     });
-    steps.push(`Total: ${total}`);
+    const totalMessage = (total) => {
+      if (total > 0) {
+        return `Você vai gastar um valor total de ${total}`;
+      } else {
+        return `Você vai receber um valor total de ${total * -1}`;
+      }
+    };
+    console.log(totalMessage);
+    steps.push(`Total: ${totalMessage(total)}`);
     setBestDealSteps(steps);
-  }
+  };
+
+  const displaySteps = (steps) => {
+    return steps.map((step) => (
+      <div>
+        <span>{step}</span>
+        <br></br>
+      </div>
+    ));
+  };
 
   return (
     <Container className="p-3">
       <Jumbotron>
         <h2 className="header">Faça suas trocas</h2>
-        <ItemForm items={localItems} updateLocalTrades={updateLocalTrades}/>
+        <ItemForm items={localItems} updateLocalTrades={updateLocalTrades} />
         <h2 className="header">Calcule o melhor preço</h2>
-        <ItemCalc items={localItems} calcBestPrice={calcBestPrice}/>
-        {bestDeal.length > 0 && bestDeal.map(step => (<div><span>{step}</span><br></br></div>))}
+        <ItemCalc items={localItems} calcBestPrice={calcBestPrice} />
+        {bestDeal.length ? displaySteps(bestDeal) : error.length ? error : null}
       </Jumbotron>
       <h3>Trocas possíveis</h3>
       <ItemTable trades={localTrades} />
