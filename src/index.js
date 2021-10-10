8b2f0755a88fdb99067a47b2926a3417afe8333c
Icaro Pereira <icarooliv2@gmail.com>
2021-10-10 17:22:19 -0300

    Lint code
    Co-authored-by: Augusto Moreno Vilarins <augustovilarins@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 // Importing the Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {buildGraph} from './server/djikstra';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { buildGraph } from "./server/util";
 
 const graph = buildGraph();
 
-ReactDOM.render(<App graph={graph}/>, document.getElementById('root'));
+ReactDOM.render(<App graph={graph} />, document.getElementById("root"));
