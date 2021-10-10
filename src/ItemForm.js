9f2fdd4e59b1b7a296df4f880e9eb0615cf077a1
Icaro Pereira <icarooliv2@gmail.com>
2021-10-10 17:24:03 -0300

    Add option to pay/receive for item
    Co-authored-by: Augusto Moreno Vilarins <augustovilarins@gmail.com>

--- src/ItemForm.js
+++ src/ItemForm.js
@@ -1,8 +1,13 @@
-import React, {useState} from 'react';
-import { Form, Button } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
 
-const ItemForm = ({items, updateLocalTrades}) => {
-  const initialInputState = { name: "", tradeTax: "", fromId:items[0].id };
+const ItemForm = ({ items, updateLocalTrades }) => {
+  const initialInputState = {
+    name: "",
+    tradeTax: "",
+    fromId: items[0].id,
+    payOrReceive: "pay",
+  };
   const [eachEntry, setEachEntry] = useState(initialInputState);
 
   const handleInputChange = (e) => {
@@ -10,55 +15,68 @@ const ItemForm = ({items, updateLocalTrades}) => {
   };
 
   const onSubmit = () => {
+    if (eachEntry.payOrReceive === "pay") {
+      eachEntry.tradeTax = eachEntry.tradeTax * -1;
+    }
     updateLocalTrades(eachEntry);
-  }
+  };
 
   return (
     <Form>
       <Form.Group>
-          <Form.Label>Eu tenho um:</Form.Label>
-          <Form.Control
-            name="name"
-            type="text"
-            className={`form-control`}
-            onChange={handleInputChange}
-          />
-        </Form.Group>
+        <Form.Label>Eu tenho um:</Form.Label>
+        <Form.Control
+          name="name"
+          type="text"
+          className={`form-control`}
+          onChange={handleInputChange}
+        />
+      </Form.Group>
 
-
-        <Form.Group>
-          <Form.Label>E aceito trocar por um:</Form.Label>
-          <Form.Control
-            name="fromId"
-            className={`form-control`}
-            onChange={
-              (e) => {
-                handleInputChange({ target: { name: "fromId", value: e.target.value } });
-              }
-            }
-            as="select"
-          >
-          {items.map(item => (
+      <Form.Group>
+        <Form.Label>E aceito trocar por um:</Form.Label>
+        <Form.Control
+          name="fromId"
+          className={`form-control`}
+          onChange={(e) => {
+            handleInputChange({
+              target: { name: "fromId", value: e.target.value },
+            });
+          }}
+          as="select"
+        >
+          {items.map((item) => (
             <option value={item.id}>{item.name}</option>
           ))}
-          </Form.Control>
-        </Form.Group>
+        </Form.Control>
+      </Form.Group>
 
-        <Form.Group>
-          <Form.Label>Com um adicional de troca:</Form.Label>
-          <Form.Control
+      <Form.Group>
+        <Form.Label>Eu quero:</Form.Label>
+        <Form.Control
+          name="payOrReceive"
+          className={`form-control`}
+          onChange={handleInputChange}
+          as="select"
+        >
+          <option value="pay">Pagar</option>
+          <option value="receive">Receber</option>
+        </Form.Control>
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Label>O valor de adicional de troca:</Form.Label>
+        <Form.Control
           name="tradeTax"
           className={`form-control`}
           onChange={handleInputChange}
-          />
-        </Form.Group>
-
-        <Button onClick ={onSubmit} className="btn btn-primary">
-          Ofertar
-        </Button>
+        />
+      </Form.Group>
+      <Button onClick={onSubmit} className="btn btn-primary">
+        Ofertar
+      </Button>
     </Form>
   );
-
-}
+};
 
 export default ItemForm;
